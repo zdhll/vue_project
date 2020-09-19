feb16ac28d34158eabd8adcf4f7ea4d1d6755b2a
vue_project <zengdh1201@163.com>
2020-09-19 22:08:13 +0800

    首页

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <!-- 轮播图 -->
+    <mt-swipe :auto="4000">
+      <mt-swipe-item v-for="item in lunbotuList" :key="item.url">
+        <img :src="item.img" />
+      </mt-swipe-item>
+    </mt-swipe>
+
+    <h3>HomeContainer</h3>
+  </div>
+</template>
+
+<script>
+import { Toast } from "mint-ui";
+
+export default {
+  data() {
+    return {
+      lunboruList: [
+        //保存轮播图的数组
+        {
+          img: "https://i.loli.net/2020/09/19/nwszEioL5WROFbl.jpg",
+          url: "https://i.loli.net/2020/09/19/nwszEioL5WROFbl.jpg",
+        },
+        {
+          img: "https://i.loli.net/2020/09/19/nwszEioL5WROFbl.jpg",
+          url: "https://i.loli.net/2020/09/19/nwszEioL5WROFbl.jpg",
+        },
+        {
+          img: "https://i.loli.net/2020/09/19/nwszEioL5WROFbl.jpg",
+          url: "https://i.loli.net/2020/09/19/nwszEioL5WROFbl.jpg",
+        },
+      ],
+    };
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.mint-swipe {
+  height: 200px;
+  .mint-swipe-item {
+    &:nth-child(1) {
+      background-color: red;
+    }
+    &:nth-child(2) {
+      background-color: green;
+    }
+    &:nth-child(3) {
+      background-color: blue;
+    }
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,33 @@
 // 入口文件
 import Vue from 'vue';
 
+//导入路由的包
+import VueRouter from 'vue-router';
+//安装路由模块
+Vue.use(VueRouter);
+
+
+//导入 vue-resource
+import VueResource from 'Vue-resource';
+Vue.use(VueResource);
+
 import './lib/mui/css/mui.min.css';
+import './lib/mui/css/icons-extra.css';
 
 //导入 App 根组件
 import app from './App.vue';
 
 //按需导入mintUI中的组件
-import { Header } from 'mint-ui';
+import { Header, Swipe, SwipeItem } from 'mint-ui';
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+//导入自己的router.js模块
+import router from './router.js';
 
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)
+    render: c => c(app),
+    router
 });
